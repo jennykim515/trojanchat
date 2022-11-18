81274a55b78089369bfec47233bf6166ef9d71c0
Jenny Kim <yjkim@usc.edu>
2022-11-17 17:54:45 -0800

    added context with user info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { createContext, useState } from "react"
+
+export const AppContext = createContext(null)
 
 function App() {
+  const [user, setUser] = useState({
+    name: "Trojan",
+    major: "Computer Science",
+    username: "tjan",
+    grad: "2024",
+    password: "password"
+  })
+
   return (
-    <h1>Ready to build</h1>
+    <AppContext.Provider value={{ user, setUser }}>
+      {/* add components */}
+    </AppContext.Provider>
   );
 }
 
