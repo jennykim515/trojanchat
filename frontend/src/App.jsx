01c72a1d1a840e8287077aaa9feb91678c942e3e
Filippo de Cristofaro <decristo@usc.edu>
2022-11-29 21:22:57 -0800

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -115,6 +115,10 @@ function App() {
 
             <Route path={'/profile'} element={<UserProfile/>}></Route>
             <Route path={'/otheruser'} element={<OtherUser/>}></Route>
+
+            <Route path={'/addthread'} element={<AddThread/>}></Route>
+
+            
             
         </Routes>
      </BrowserRouter> 
@@ -163,4 +167,4 @@ function DemoComponent() {
             <button onClick={logOut}>Log out</button>
         </div>
     );
-}
+}git
